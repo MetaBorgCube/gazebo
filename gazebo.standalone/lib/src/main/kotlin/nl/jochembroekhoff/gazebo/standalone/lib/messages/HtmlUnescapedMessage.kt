301d2552f56a4337665660a6a42c7eb35ddf56d2
Jochem Broekhoff <broekhoff.jochem@gmail.com>
2022-03-05 13:51:15 +0100

    [standalone] Make string replacement in HtmlUnescapedMessage more efficient

--- gazebo.standalone/lib/src/main/kotlin/nl/jochembroekhoff/gazebo/standalone/lib/messages/HtmlUnescapedMessage.kt
+++ gazebo.standalone/lib/src/main/kotlin/nl/jochembroekhoff/gazebo/standalone/lib/messages/HtmlUnescapedMessage.kt
@@ -1,18 +1,17 @@
 package nl.jochembroekhoff.gazebo.standalone.lib.messages
 
+import org.apache.commons.lang3.StringUtils
 import org.metaborg.core.messages.IMessage
 
 class HtmlUnescapedMessage(private val message: IMessage) : IMessage by message {
 
     companion object {
+
+        private val replaceSearch = arrayOf("&gt;", "&lt;", "&nbsp;", "<br>\n", "<br>")
+        private val replaceReplacement = arrayOf(">", "<", " ", "\n", "\n")
+
         fun unescape(message: String): String {
-            // TODO: efficient replace
-            return message
-                .replace("&gt;", ">")
-                .replace("&lt;", "<")
-                .replace("&nbsp;", " ")
-                .replace("<br>\n", "\n")
-                .replace("<br>", "\n")
+            return StringUtils.replaceEach(message, replaceSearch, replaceReplacement)
         }
     }
 
