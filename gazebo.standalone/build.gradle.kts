a006edbcdb71af95dfd4077df95039796cacf1f5
Jochem Broekhoff <broekhoff.jochem@gmail.com>
2022-05-14 11:22:20 +0200

    [standalone] Bump Kotlin version

--- gazebo.standalone/build.gradle.kts
+++ gazebo.standalone/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     base
     java
-    kotlin("jvm") version "1.6.10" apply false
+    kotlin("jvm") version "1.6.20" apply false
 }
 
 allprojects {
